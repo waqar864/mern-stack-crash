b39d8da77a340bbfda31e1e6078c948ca15361b5
waqar864 <waqarkhan864@gmail.com>
2024-10-22 02:02:26 +0500

    mearn-stack

---
+++ backend/models/product.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+},
+{
+    timestamps: true // created at updated at time
+});
+
+const Product = mongoose.model("Product", productSchema);
+export default Product
